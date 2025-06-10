88b0db37f75c0c5d7436ac4647625ac69454ec13
nitishthorat <nitishthorat112@gmail.com>
2025-06-10 15:52:30 -0700

    Added Note create function

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
+        @session('message')
+            <div class="success-message">
+                {{session('message')}}
+            </div>
+        @endsession
         {{$slot}}
     </body>
 </html>
--- resources/views/note/create.blade.php
+++ resources/views/note/create.blade.php
@@ -2,10 +2,11 @@
     <div class="note-container single-note">
         <h1>Create New Note</h1>
         <form action="{{route('note.store')}}" method="POST" class="note">
+            @csrf
             <textarea name="note" class="note-body" id="" cols="30" rows="10" placeholder="Enter your note here"></textarea>
             <div class="note-buttons">
                 <a href="{{route('note.index')}}" class="note-cancel-button">Cancel</a>
-                <a href="class-submit-button">Submit</a>
+                <button type="submit" class="note-submit-button">Submit</button>
             </div>
         </form>
     </div>
